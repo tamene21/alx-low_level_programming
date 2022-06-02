7d7a50ea47945dd88945e578722d121c6b56302d
tamene Gelaye <tamenes21@gmail.com>
2022-06-02 14:02:01 -0700

    new commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,13 +7,11 @@
 
 int main(void)
 {
-	printf("char size: %d byte(s)\n", sizeof(char));
-	printf("int size: %d byte(s)\n", sizeof(int));
-	printf("short size: %d byte(s)\n", sizeof(short));
-	printf("float size: %d byte(s)\n", sizeof(float));
-	printf("long size: %d byte(s)\n", sizeof(long));
-	printf("DOuble size: %d byte(s)\n", sizeof(double));
-	printf("long double size: %d byte(s)\n", sizeof(long double));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
 }

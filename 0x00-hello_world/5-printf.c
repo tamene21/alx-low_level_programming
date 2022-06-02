06a6a8e3ceb120ad16bbfbb772ecd4fa1bf53372
tamene Gelaye <tamenes21@gmail.com>
2022-06-02 11:35:04 -0700

    new commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -prints 'with proper grammer , ....'
+ * Return:0 if the program runs successfuly else otherwise
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art");
+	return (0);
+
+}

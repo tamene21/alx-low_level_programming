7544b4a78dc0b53bdcacd3b3564213278a77788e
tamene Gelaye <tamenes21@gmail.com>
2022-06-07 10:06:45 -0700

    replicating a to z tten time

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - prints 10 times the alphabet, in lowercase, followed by a new line.
+ * Return: Always 0 (success).
+ */
+
+void print_alphabet_x10(void);
+{
+char ch;
+int i = 0;
+
+while (i < 10)
+{
+ch = 'a';
+while (ch <= z)
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+}

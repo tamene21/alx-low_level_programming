ea9ca16aad4c9bbc8fa296fdf46f6b9d941d801f
tamene Gelaye <tamenes21@gmail.com>
2022-06-07 10:46:53 -0700

    edited

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (success).
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char ch;
 	int i = 0;
@@ -20,4 +20,4 @@ void print_alphabet_x10(void);
 		}
 		_putchar('\n');
 	}
- }
+}

298ec91a262daaf510137d7be63ecd0ed1525fe4
tamene Gelaye <tamenes21@gmail.com>
2022-06-07 10:42:50 -0700

    edited

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,17 @@
 
 void print_alphabet_x10(void);
 {
-char ch;
-int i = 0;
+	char ch;
+	int i = 0;
 
-while (i < 10)
-{
-ch = 'a';
-while (ch <= z)
-{
-_putchar(ch);
-ch++;
-}
-_putchar('\n');
-}
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= z)
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+	}
+ }

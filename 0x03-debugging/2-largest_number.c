e03e8f61f573fcd71f3a149973d7062993e6d129
Tamene Gelaye <tamenes21@gmail.com>
2022-06-09 05:13:50 -0700

    new cmmit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,15 +14,16 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && b > c)
 	{
-	largest = a;
+		largest = a;
 	}
 	else if (b > a && a > c)
 	{
-	largest = b;
+		largest = b;
 	}
 	else
 	{
 		largest = c;
 	}
+
 	return (largest);
 }

7ece8ba296bb20e519ec8bb63f480aaa1984788f
Tamene Gelaye <tamenes21@gmail.com>
2022-06-16 07:38:00 -0700

    New commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strcmp - performs a binary comparision of s1 and s2
+ *@s1: string 1
+ *@s2: string 2
+ *Return: The difference in binary between the two strings
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1)
+	{
+		if (*s1 != s2)
+			break;
+		s1++;
+		s2++;
+	}
+
+	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
+
+}

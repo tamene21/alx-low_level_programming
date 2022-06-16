7615ad73b2bd443c12489b822bf05cef0ae96d1f
Tamene Gelaye <tamenes21@gmail.com>
2022-06-16 08:03:18 -0700

    new commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1)
+	while (*s1 && (*s1 == *s2))
 	{
-		if (*s1 != s2)
-			break;
 		s1++;
 		s2++;
 	}
 
-	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
+	return (*s1 - *s2);
 
 }

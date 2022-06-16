655dbb1dc2e1c5a457890c95e29374a8a31c376d
Tamene Gelaye <tamenes21@gmail.com>
2022-06-16 13:26:54 -0700

    new commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes the available array to uppercase
+ * @s: string to be chnaged
+ * Return: upper case array
+ */
+
+char *string_toupper(char *)
+{
+
+
+
+}

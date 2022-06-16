8dcb53a6b0be8bbadcf514f1ba998b4bb1ce32ea
Tamene Gelaye <tamenes21@gmail.com>
2022-06-16 04:59:14 -0700

    New commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	int i, j;
 
-	for (i = o; dest[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 		;
 	for (j = 0; j < n && src[j] != '\0'; j++)
 	{

d2fd57051bad7d756a4c99b3a409e375a92076de
Tamene Gelaye <tamenes21@gmail.com>
2022-06-16 04:42:27 -0700

    New commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *_strncat - concatinating dest on src within n places
+ *
+ *@dest:This is destination file
+ *@src:This is source file
+ *@n:No of places for dest file after src
+ *
+ * Return: Returning dest after concatenating in n places
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int i, j, n;
+
+	for (i = o; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j] = n;
+		j++;
+	}
+
+	return (dest);
+
+}

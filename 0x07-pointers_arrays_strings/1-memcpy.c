9a3c1b894b3331cea07745335b2d25efeab3655a
Tamene Gelaye <tamenes21@gmail.com>
2022-06-20 14:49:54 -0700

    copying a memory area from source to destination

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copying bytes from memory area to dest
+ * @dest: A memory area to be replaced
+ * @src: A menmory area to be compied
+ * @n: a number of bytes
+ * Return: Returning dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src;
+	}
+	return (dest);
+}

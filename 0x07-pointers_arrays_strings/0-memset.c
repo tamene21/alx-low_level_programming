b88c407ff4525a9b3abcad8147db0f2c8d6560a1
Tamene Gelaye <tamenes21@gmail.com>
2022-06-20 06:39:14 -0700

    new commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_memset - fill a memory with a constant byte
+ *@s:pointer variable to the value
+ *@b:value to be filled in the memory
+ *@n:size of the value
+ *
+ * Return: point to the memory area s
+ */
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (0);
+
+}

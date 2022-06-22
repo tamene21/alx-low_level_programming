861257b194c84aa128d5fadadeb99afab548dcca
Tamene Gelaye <tamenes21@gmail.com>
2022-06-22 15:32:30 -0700

    new commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion -prints astrings length
+ * @s: string argument
+ * @i: string length
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+	return (i);
+}

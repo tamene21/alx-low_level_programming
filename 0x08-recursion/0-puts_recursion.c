c73f83780bfe867ee775ad736a06093c877c9213
Tamene Gelaye <tamenes21@gmail.com>
2022-06-22 05:05:07 -0700

    printing strings with a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_puts_recursion - print a string with a new line
+ *@s: input argument
+ *Return: Always 0 for success
+ */
+void _puts_recursion(char *s)
+{
+	printf("%s\n");
+
+	return (0);
+}

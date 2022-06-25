cb591be0578eb6d7b34fb207b35b91603434acb5
Tamene Gelaye <tamenes21@gmail.com>
2022-06-25 02:19:05 -0700

    listing arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ *main - program name
+ *@argc: argument size
+ *@argv: argument value
+ *Return: always 0 for success
+ */
+int main(int argc, *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		argv[i] = argc[i]
+	}
+	printf("argv[i]: %s\n", i, argv[i]);
+
+	return (0);
+
+
+
+}

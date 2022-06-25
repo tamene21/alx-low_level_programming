afb8431c8d73f216be699bba064e4b60e8415749
Tamene Gelaye <tamenes21@gmail.com>
2022-06-25 01:42:32 -0700

    new commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,12 +8,11 @@
  */
 int main(int argc, char *argv[])
 {
-        int i;
+	int i;
 
 	for (i = 0; i < argc; i++)
-        {
-                printf("%s\n", argv[0]);
-        }
-
+	{
+		printf("%s\n", argv[0]);
+	}
 	return (0);
 }

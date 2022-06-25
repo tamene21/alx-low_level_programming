466072104a4d725acdd1837cf3076881e8ace665
Tamene Gelaye <tamenes21@gmail.com>
2022-06-25 01:15:52 -0700

    New commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,17 @@
+#include "main.h"
 #include<stdio.h>
 #include<stdilib.h>
 /**
+ *main - prints the name of the program
+ *@argc: number of arguments
+ *@argv: vector of argument
  *
- *
- *
- *
- *
+ *Return: Always 0 (success)
  */
-int main (argc, char **argv[])
-{
-printf("%s\n",argv[0]);
 
-return 0;
+int main(argc, char **argv[])
+{
+	printf("%s\n", *argv[0]);
+
+	return (0);
 }

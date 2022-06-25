4bbb9ebff71703ff6f9bdab8de1b69b32e732182
Tamene Gelaye <tamenes21@gmail.com>
2022-06-25 04:12:40 -0700

    New commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,30 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- *main - a program adds two positive numbers
- *@argc: size of arguments
- *@argv: vector of argumnts
- *Return: 0 for not passed & 1 not int
+ * main- - adds positive numbers
+ * @argc: size of argv
+ * @argv: array of size argc
+ * Return: 0 if successful 1 if error
  */
 int main(int argc, char *argv[])
 {
-	int sum;
-	int x;
-	int y;
+	int sum, x;
+	char *y;
+	int val;
 
-	if (argv[1] && argv[2])
+	sum = 0;
+	if (argc > 1)
 	{
-		x = atoi(argv[1]);
-		y = atoi(argv[2]);
-		sum = x + y;
-
-		printf("%d\n", sum);
+		for (x = 1; argv[x]; x++)
+		{
+			val = strtol(argv[x], &y, 10);
+			if (!*y)
+				sum += val;
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 	}
-	else if ((argv[1] && argv[2]) == NULL)
-		return ("0\n"); 
-	else if ((argv[1] && argv[2]) != int)
-		printf("Error\n");
-	return (1);
+	printf("%d\n", sum);
+	return (0);
 }
-

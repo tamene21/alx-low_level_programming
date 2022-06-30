2efa45c88087ce25da037115d1cd6f29715a4cdc
Tamene Gelaye <tamenes21@gmail.com>
2022-06-30 10:42:57 -0700

    new commit of malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include<stdlib.h>
+
+/**
+ * malloc_checked - a function to allocate memory using malloc
+ * @b: Intiger for number of elements
+ * Return:A pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *s;
+
+	s = malloc(sizeof(int) * b);
+
+	if (s == NULL)
+		exite(98);
+
+	free(s);
+
+}

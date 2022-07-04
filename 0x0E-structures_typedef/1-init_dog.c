98f293fb8291e534a649919852272d155f99b5e0
Tamene Gelaye <tamenes21@gmail.com>
2022-07-04 12:50:50 -0700

    New commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - struct dog variable
+ *@d: struct dog
+ *@name:name of the dog
+ *@age:age of the dog
+ *@owner: owners name
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	if (d != NUL)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+	}
+
+}

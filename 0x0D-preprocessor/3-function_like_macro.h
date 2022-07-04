a144e490c4e49e1629089389a3402250fb9d8898
Tamene Gelaye <tamenes21@gmail.com>
2022-07-04 08:33:47 -0700

    a macro for absolut value computation

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,8 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x)  (x * ((x < 0) * (-1) + (x > 0)))
+
+#define ABS(x) (x * ((x < 0) * (-1) + (x > 0)))
 
 
-#endif /* FUNCTION_LIKE_MACRO_H */
+#endif /*FUNCTION_LIKE_MACRO_H */

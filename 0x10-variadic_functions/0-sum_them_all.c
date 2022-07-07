21636bfd776146e3e914193fb7ca5c631c90a71f
Tamene Gelaye <tamenes21@gmail.com>
2022-07-07 04:55:16 -0700

    Suming all argumnets

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,32 @@
+#include "vardiac_function.h"
+#include<stdarg.h>
+#include<stdio.h>
+
+/**
+ * sum_them_all - suming the number of argumnets
+ * @n: a counter for argumnets
+ * Return: sum otherwise 0 for n is 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+	va_list ap;
+	int i, sum;
+
+	va_start(ap, n);
+
+	sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, n);
+		va_end(ap);
+		return (sum);
+	}
+
+
+}

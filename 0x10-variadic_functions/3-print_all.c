6a6680cf2149c2b4c91afa9954231bbc350450a3
Tamene Gelaye <tamenes21@gmail.com>
2022-07-07 08:46:14 -0700

    draft

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+#include<strings.h>
+#include<stdarg.h>
+#include<stdio.h>
+
+/**
+ * print_all - print all data types
+ * @format: data types or farms
+ * Return: Nothing 
+ */
+
+void print_all(const char * const format, ...)
+{
+va_list all_type;
+
+
+
+printf("\n");
+
+va_end(all_type);
+}

619d77733b1fa59fbcf9ab595db851d01aa682d7
Tamene Gelaye <tamenes21@gmail.com>
2022-07-07 07:36:53 -0700

    new amendment

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,10 +15,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ap, n);
-	for (i = 0; i <= n; i = va_arg(ap, int))
-		if (separator == NULL)
 
-			printf("%d", i);
+	for (i = 0; i < n; i++)
+		printf("%d", va_arg(ap, int));
+
+	if (separator != NULL && i != n - 1)
+		printf("%s", separator);
+
 
 	printf("\n");
 

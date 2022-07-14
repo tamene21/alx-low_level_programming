c7394644e00a81b4aa1e179921b453a812f52c32
Tamene Gelaye <tamenes21@gmail.com>
2022-07-14 04:40:07 -0700

    change in description

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,12 +1,17 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 /**
  * struct list_s -singly liked list
  * @str: string
  * @len: lenght of the string
  * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
 
 typedef struct list_s

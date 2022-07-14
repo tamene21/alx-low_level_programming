9964a3760b421f5704600730ef0d1dc30896f83d
Tamene Gelaye <tamenes21@gmail.com>
2022-07-14 04:42:49 -0700

    fixed prototype

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,18 +2,17 @@
 #define LISTS_H
 
 #include <stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * struct list_s -singly liked list
- * @str: string
- * @len: lenght of the string
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  */
-
 typedef struct list_s
 {
 	char *str;
@@ -21,11 +20,12 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+int _strlen(const char *);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
-size_t print_list(const list_t *h)
-size_t list_len(const list_t *h)
-list_t *add_node(list_t **head, const char *str)
-list_t *add_node_end(list_t **head, const char *str)
-void free_list(list_t *head)
 
-#endif /*LISTS-H*/
+#endif /*LISTS_H */

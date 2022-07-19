395940a5b18058db2b5976e1b06395725bd39c21
Tamene Gelaye <tamenes21@gmail.com>
2022-07-19 04:29:55 -0700

    liberaries added

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- *print_listint - print element of the function
- *@h: top of the node
- *Return: elements
+ *print_listint - print all the element of the list 
+ *@h: pointer to the head of the list
+ *Return: number of elements
  */
 
 size_t print_listint(const listint_t *h)

8eb5d186eca9025aae0a5993b53da01a5a21f4e9
Tamene Gelaye <tamenes21@gmail.com>
2022-07-19 02:53:40 -0700

    printing elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,14 @@
 
 size_t print_listint(const listint_t *h)
 {
+	size_t elm = 0;
+
 	while (h != NULL)
 	{
-		printf("[h]", h->next);
+		printf("%d\n", h->next);
 		h = h->next;
+		elm++;
 	}
 
-	return (h);
+	return (elm);
 }

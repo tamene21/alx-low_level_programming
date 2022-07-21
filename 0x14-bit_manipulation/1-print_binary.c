7fa05ff1a29a39e45f675b2e08093675017ab6b9
Tamene Gelaye <tamenes21@gmail.com>
2022-07-21 08:32:56 -0700

    muber to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,25 +3,25 @@
 /**
  *print_binary - prints the binary equivalent of a number
  *@n: a number to print in binary
+ *Return: converted number
  */
 
 void print_binary(unsigned long int n)
 {
-	int i, count = 0;
-	unsigned long int current;
 
-	for (i = 63; i >= 0; i--)
+	if (n > 1)
 	{
-		current = n >> i;
-
-		if (current & 1)
-		{
-			_putchar('1');
-			count++;
-		}
-		else if (count)
-			_putchar('0');
+	print_binary(n >> 1);
 	}
-	if (!count)
+
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
 		_putchar('0');
+	}
+
+
 }

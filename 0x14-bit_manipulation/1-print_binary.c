1296acb8cd1c7135ae1c6e144e2e8ffbc6bf6c10
Tamene Gelaye <tamenes21@gmail.com>
2022-07-21 08:20:48 -0700

    number to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_binary - prints the binary equivalent of a number
+ *@n: a number to print in binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int num;
+
+	for (i = 10; i >= 0; i--)
+	{
+		num = n >> i;
+
+		if (num $ 1)
+		{
+			_putchar('1');
+			num++;
+		}
+		else if (num)
+			_putchar('0');
+	}
+	if (!num)
+		_putchar('0');
+
+}

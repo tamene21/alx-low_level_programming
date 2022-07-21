324d6e5985380a7f7ccc9a82c433ee5d45b9a46d
Tamene Gelaye <tamenes21@gmail.com>
2022-07-21 08:28:56 -0700

    number to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,13 +8,13 @@
 void print_binary(unsigned long int n)
 {
 	int i, count = 0;
-	unsigned long int num;
+	unsigned long int current;
 
-	for (i = 10; i >= 0; i--)
+	for (i = 63; i >= 0; i--)
 	{
-		num = n >> i;
+		current = n >> i;
 
-		if (num & 1)
+		if (current & 1)
 		{
 			_putchar('1');
 			count++;
@@ -22,8 +22,6 @@ void print_binary(unsigned long int n)
 		else if (count)
 			_putchar('0');
 	}
-
 	if (!count)
 		_putchar('0');
-
 }
